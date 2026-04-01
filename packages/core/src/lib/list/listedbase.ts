64a36c2891dd80a225ac0c592204c88596f019f6
GUMRI <37027572+GUMRI@users.noreply.github.com>
2026-04-01 19:59:14 +0000

    Auto-commit

--- packages/core/src/lib/list/listedbase.ts
+++ packages/core/src/lib/list/listedbase.ts
@@ -46,7 +46,7 @@ export function list<S extends TSchemaRef>(
   const manyFromFields = Object.entries(props).filter(([key, value]: any) => value.manyFrom).map(([key, value]) => ({ [key]: value }))
   console.log(manyFromFields);
 
-  return Object.assign({ items: store. }, {
+  return Object.assign({ items: store.value }, {
 
 
     async create(input: LCreateInput<S>) {
