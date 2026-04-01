e04305294687447a20b3c0d12d1268bf354a59cc
GUMRI <37027572+GUMRI@users.noreply.github.com>
2026-04-01 19:43:17 +0000

    Auto-commit

--- playgrounds/angular/angular-plg/src/app/schemas/post.ts
+++ playgrounds/angular/angular-plg/src/app/schemas/post.ts
@@ -6,7 +6,7 @@ export const sPosts = lSchema({
     name: "posts",
     fields: {
         id: lField.id.autouuid(),
-        obj: lField.object({ one: z.string() }),
+        obj: lField.sobject({ one: z.string() }),
         user: () => lField.oneFrom(sUser)
     }
 })
