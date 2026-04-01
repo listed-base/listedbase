efe6c686da2909247a955a7c68d0d651956f2415
GUMRI <37027572+GUMRI@users.noreply.github.com>
2026-04-01 18:59:03 +0000

    Auto-commit

--- packages/core/src/lib/schema/field.ts
+++ packages/core/src/lib/schema/field.ts
@@ -9,7 +9,7 @@
  */
 import * as z from 'zod/v4';
 import { addMeta } from "./meta";
-import { TSchemaRef } from './schema-class';
+import { TSchemaRef } from './schema.ts';
 
 type IndexType = z.ZodString | z.ZodNumber | z.ZodDate | z.ZodBigInt;
 
