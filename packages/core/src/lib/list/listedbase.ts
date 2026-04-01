7d1a36b95a005ce43097b3731248c4b7b4703dc5
GUMRI <37027572+GUMRI@users.noreply.github.com>
2026-04-01 19:33:10 +0000

    Auto-commit

--- packages/core/src/lib/list/listedbase.ts
+++ packages/core/src/lib/list/listedbase.ts
@@ -2,7 +2,7 @@ import { Item } from './../../duct/types';
 import { z, ZodObject, ZodRawShape } from 'zod/v4';
 import * as v4 from 'zod/v4/core';
 import { TSchemaRef } from '../schema/schema';
-import { LCreateInput, LCreateInput, LItem } from '../schema/typing/main';
+import { LCreateInput, LCreateInput, LItem, LUpdateInput } from '../schema/typing/main';
 
 export type ListEventType = 'create' | 'update' | 'remove';
 
