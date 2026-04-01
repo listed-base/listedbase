fdd474dc7d4f11e737ca45223f0de8f0a0760555
GUMRI <37027572+GUMRI@users.noreply.github.com>
2026-04-01 20:08:29 +0000

    Auto-commit

--- packages/core/src/lib/list/base.ts
+++ packages/core/src/lib/list/base.ts
@@ -1,6 +1,5 @@
 import { createReactive } from "@listedbase/uni-reactive";
 import { z, ZodObject, ZodRawShape } from "zod/v4";
-// import { SchemaRef } from "../schema/schema";
 
 export class BaseList<TOutput, TShap extends ZodRawShape> {
     protected state = createReactive<TOutput[]>();
