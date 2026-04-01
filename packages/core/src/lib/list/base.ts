db8371bffc6ba02952265cc2da24f757a7439c89
GUMRI <37027572+GUMRI@users.noreply.github.com>
2026-04-01 20:08:51 +0000

    Auto-commit

--- packages/core/src/lib/list/base.ts
+++ packages/core/src/lib/list/base.ts
@@ -5,7 +5,7 @@ export class BaseList<TOutput, TShap extends ZodRawShape> {
     // protected state = createReactive<TOutput[]>();
     protected schema!: any;
     constructor() {
-        this.state.init([]);
+        // this.state.init([]);
     }
 
     items = this.state.value
