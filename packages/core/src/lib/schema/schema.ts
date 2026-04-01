6a71e71b5ff90d2dc72af47532b2086a64b26c4e
GUMRI <37027572+GUMRI@users.noreply.github.com>
2026-04-01 18:58:33 +0000

    Auto-commit

--- packages/core/src/lib/schema/schema.ts
+++ packages/core/src/lib/schema/schema.ts
@@ -1,180 +1,109 @@
-import { object, z, ZodAny, ZodObject, ZodRawShape } from "zod/v4";
-
-// export const lRef = <TShape extends ZodRawShape>(fn: SchemaRef<TShape>) => fn
-// interface LSchemaOptions<TShape extends ZodRawShape> {
-//   name: string;
-//   fields: () => ZodType | ZodType;
-// }
-
-export type LField =
-  | z.ZodTypeAny
-  | (() => z.ZodTypeAny);
-
-export type LRawShape = Record<string, LField>;
-
-export class SchemaRef<TShape extends ZodRawShape> {
-  schema: ZodObject<TShape>
-  constructor(public name: string,
-    public fields: TShape) {
-    this.schema = z.object(fields)
-  }
+import { never, z, ZodLazy, ZodObject, ZodUnion } from "zod/v4";
+import core, { util } from "zod/v4/core";
+interface TSchemaOptions<TName extends string, TFields extends core.$ZodLooseShape> {
+    name: TName,
+    fields: TFields
 }
-// export function lSchema<TShape extends ZodRawShape>(
-//   { name, fields }: LSchemaOptions<TShape>
-// ): z.ZodObject<TShape> {
 
-//   const schema = z.lazy(() => z.object(fields)).meta({ name }) as unknown as z.ZodObject<TShape>;
-
-//   return schema
-// }
-
-
-type FieldFactory<T extends ZodAny> =
-  | T
-  | (() => T)
-
-type UnwrapField<T> =
-  T extends () => infer R
-  ? R
-  : T;
-
-type ResolveShape<T extends LRawShape> = {
-  [K in keyof T]: UnwrapField<T[K]>;
+type ResolveLazy<T> =
+    T extends () => infer R
+    ? ResolveLazy<R>
+    : T;
+type ResolveFields<T> = {
+    [K in keyof T]: ResolveLazy<T[K]>;
 };
 
-function resolveShape<T extends LRawShape>(
-  shape: T
-): ResolveShape<T> {
+/**
+ * Extract first schema from ZodUnion
+ */
+type FirstFromZodUnion<T> =
+    T extends z.ZodUnion<infer Options>
+    ? Options extends [infer First, ...any[]]
+    ? First
+    : never
+    : T;
 
-  const resolved: Record<string, z.ZodTypeAny> = {};
-
-  for (const key in shape) {
-
-    const value = shape[key];
-
-    resolved[key] =
-      typeof value === "function"
-        ? value()
-        : value;
-  }
-
-  return resolved as ResolveShape<T>;
+/**
+ * Apply on object fields
+ */
+type PickFirstUnionFields<T> = {
+    [K in keyof T]: FirstFromZodUnion<T[K]>;
+};
+export interface TSchemaRef<S extends ZodObject = ZodObject, Z extends ZodObject = ZodObject> {
+    schema: S
+    zodSchema: Z,
+    // jsonSchema: core.ZodStandardJSONSchemaPayload<S> & { idField: string }
 }
-export type TSchemaMap<S extends z.ZodObject> = Map<string, S>
- const schemaRegistry = <S extends z.ZodObject>(): TSchemaMap<S> => new Map<string, S>();
+export const registerSchema = new Map<string, ZodObject>()
+export function lSchema<
+    TName extends string,
+    TFields extends Record<string, any>
+>(
+    { name, fields }: TSchemaOptions<TName, TFields>
+): TSchemaRef<ZodObject<ResolveFields<TFields>, core.$strict>, ZodObject<ResolveFields<PickFirstUnionFields<TFields>>, core.$strict>> {
 
-export class LSchema<TShape extends LRawShape = LRawShape> {
+    const resolvedShape: any = {};
+    let hasIdFiled = false
+    for (const key in fields) {
+        const value = fields[key];
 
-  private resolved: ResolveShape<TShape>
-  public schema: z.ZodObject
+        if (typeof value === "function") {
+            resolvedShape[key] = z.lazy(() => value().options[0])
 
-  constructor(
-    public name: string,
-    public fields: TShape
-  ) {
-    this.resolved = resolveShape(fields);
-    this.schema = z.object(this.resolved)
-  }
+        } else {
+            resolvedShape[key] = value;
+        }
+
+        const field = resolvedShape[key];
 
 
+        if (field instanceof ZodUnion) {
+            if (field.options[1] instanceof ZodObject && field.options[1].shape.hasOwnProperty("idField")) {
+                hasIdFiled = true
+            }
+            resolvedShape[key] = field.options[0];
+        }
 
-}
-export function lSchema<TShape extends LRawShape>(
-  name: string,
-  fields: TShape
-) {
-
-  const resolved = resolveShape(fields);
-
-  const schema = z.object(resolved).meta({ name });
-  schemaRegistry().set(name, schema)
-
-  return schema;
-}
-
-function flattenAnyOfFirst(schema: any) {
-  if (!schema || typeof schema !== "object") return schema;
-
-  // If this node is an anyOf wrapper, replace it with anyOf[0]
-  if (Array.isArray(schema.anyOf) && schema.anyOf.length > 0) {
-    return flattenAnyOfFirst(schema.anyOf[0]);
-  }
-
-  // Recurse common JSON Schema containers
-  if (schema.properties && typeof schema.properties === "object") {
-    for (const key of Object.keys(schema.properties)) {
-      schema.properties[key] = flattenAnyOfFirst(schema.properties[key]);
     }
-  }
 
-  if (schema.items) {
-    schema.items = flattenAnyOfFirst(schema.items);
-  }
+    if (!hasIdFiled) {
+        throw new Error(`listedbase:Schema ${name} must have an id field`);
+    }
 
-  if (schema.$defs && typeof schema.$defs === "object") {
-    for (const key of Object.keys(schema.$defs)) {
-      schema.$defs[key] = flattenAnyOfFirst(schema.$defs[key]);
-    }
-  }
-  const indexesFields = []
-  const uniqueFileds = []
-  if (schema.properties && typeof schema.properties === "object") {
-    for (const key of Object.keys(schema.properties)) {
-      if (schema.properties[key]["indexe"]) indexesFields.push(key)
-      if (schema.properties[key]["unique"]) uniqueFileds.push(key)
-      if (schema.properties[key]["idField"])
-        schema["idField"] = key
-    }
-  }
-  return schema;
+    const def: core.$ZodObjectDef = {
+        type: "object",
+        shape: resolvedShape,
+        catchall: never(),
+    };
+
+    const schema = (z.object(resolvedShape) as any).meta({ name });
+    z.globalRegistry.add(schema, { id: name })
+
+    return new SchemaRef
+        <ZodObject<ResolveFields<TFields>, core.$strict>, ZodObject<ResolveFields<PickFirstUnionFields<TFields>>, core.$strict>
+        >(schema, schema)
 }
 
-// usage
-// const jsonSchema = z.toJSONSchema(myZodSchema);
+class SchemaRef<S extends ZodObject, Z extends ZodObject> implements TSchemaRef<S, Z> {
+    schema: S
+    zodSchema: Z
+    constructor(schema: S, zodSchema: Z) {
+        this.schema = schema
+        this.zodSchema = zodSchema
+    }
 
-// if you only want the specific one-liner behavior for properties:
-// for (const key of Object.keys(jsonSchema.properties ?? {})) {
-//   const prop = jsonSchema.properties[key];
-//   if (prop?.anyOf?.[0]) jsonSchema.properties[key] = prop.anyOf[0];
-// }
-
-// const userSchema = lSchema({
-//   name: "User",
-//   fields: {
-//     id: lField.id.autouuid(),
-//     name: z.string(),
-//     age: z.number().optional(),
-//   },
-// });
-// type User = z.output<typeof userSchema.schema>;
-
-// type UserInput = OmitMarkedAutoFields<z.input<typeof userSchema.schema>>; 
-
-// const userInput: UserInput = {
-//   name: "Alice",
-//   age: 30,
-// };
+}
 
 
 
 
 
 
-// function listFactory<S extends z.ZodObject>(ref: SchemaRef<S>) {
-//   type Item = z.output<S>;
-//   type ItemInput = z.input<S>;
 
-//   const parseItem = (v: unknown): Item => ref.schema.parse(v);
 
-//   return {
-//     items: [] as Item[],
-//     parseItem,
-//     create(input: ItemInput): Item {
-//       // if you want defaults/transforms applied, parse is a good “constructor”
-//       return ref.schema.parse(input);
-//     },
-//   };
-// }
+
+
+
 
 
 
