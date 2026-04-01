d3f538d834a8f1c8186cebf0244ad38fddab1aae
GUMRI <37027572+GUMRI@users.noreply.github.com>
2026-04-01 18:59:52 +0000

    Auto-commit

---
+++ packages/core/src/lib/schema/meta.ts
@@ -0,0 +1,24 @@
+import z, { keyof, ZodAny, ZodLiteral, ZodType } from "zod/v4"
+
+export type TMeta<T = boolean> = {
+    idField?: T,
+    autogen?: T,
+    unique?: T,
+    index?: T,
+    oneFrom?: T,
+    manyFrom?: T,
+    date?: T
+}
+
+const register = z.registry<TMeta>()
+
+type RequiredMeta<T> = { [P in keyof T]-?: ZodLiteral<true>; }
+
+export function addMeta<T extends ZodType, C extends TMeta>(schema: T, meta: C) {
+    let marks = {} as RequiredMeta<C>
+    for (const key in meta) {
+        (marks as any)[key] = z.literal(true)
+    }
+    return schema.meta({ ...meta }).or(z.object(marks))
+
+}   
