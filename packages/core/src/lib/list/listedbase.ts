23955b4022f67e5cabf185dc7b77cf4c9e1bc255
GUMRI <37027572+GUMRI@users.noreply.github.com>
2026-04-01 20:11:05 +0000

    Auto-commit

--- packages/core/src/lib/list/listedbase.ts
+++ packages/core/src/lib/list/listedbase.ts
@@ -37,7 +37,7 @@ function applySelect<T extends object, S extends SelectConfig<T>>(
 export function list<S extends TSchemaRef>(
   schema: S,
 ) {
-  const store = new lIS
+  const store = new B
   type Item = LItem<S>
   const json = schema.schema.toJSONSchema()
   const props = json.properties as any
