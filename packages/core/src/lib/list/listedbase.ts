23c712462c9ea53229b25f2fbae567e2c09455a4
GUMRI <37027572+GUMRI@users.noreply.github.com>
2026-04-01 20:01:43 +0000

    Auto-commit

--- packages/core/src/lib/list/listedbase.ts
+++ packages/core/src/lib/list/listedbase.ts
@@ -1,4 +1,4 @@
-import { createReactive } from '../../../../../../uni-reactive/core/src/index';
+import { createReactive } from '@listedbase/uni-reactive';
 import { TSchemaRef } from '../schema/schema';
 import { LCreateInput, LItem, LUpdateInput, LWhereUniqueInput } from '../schema/typing/main';
 
