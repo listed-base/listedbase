5f81d4f2f24fb8fa60e8341bf7f3065ea13877c9
GUMRI <37027572+GUMRI@users.noreply.github.com>
2026-04-02 15:24:46 +0000

    Auto-commit

--- packages/core/src/lib/list/listedbase.ts
+++ packages/core/src/lib/list/listedbase.ts
@@ -74,7 +74,7 @@ export function list<S extends TSchemaRef>(
       return { ...input } as Item
     },
 
-    findMany<S extends ShepeOptions<Item>>(config?: { select: S }) {
+    findMany<T extends ShepeOptions<Item>>(config?: { select: S }) {
      
       return store.value
     },
