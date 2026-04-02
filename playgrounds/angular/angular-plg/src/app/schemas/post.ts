0a8159a482af9b33a1090842392fb420bbba43c5
GUMRI <37027572+GUMRI@users.noreply.github.com>
2026-04-02 10:44:29 +0000

    Auto-commit

--- playgrounds/angular/angular-plg/src/app/schemas/post.ts
+++ playgrounds/angular/angular-plg/src/app/schemas/post.ts
@@ -1,6 +1,5 @@
 import { lField, lSchema, list } from '@listedbase/core';
 import { sUser } from './user';
-import { LItem } from 'packages/core/src/lib/schema/typing/itme';
 
 export const sPosts = lSchema({
     name: "posts",
