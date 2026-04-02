c5bece1c6ceb89338a8e1a2517be47276c71718e
GUMRI <37027572+GUMRI@users.noreply.github.com>
2026-04-02 15:20:40 +0000

    Auto-commit

--- packages/core/src/lib/schema/typing/result.ts
+++ packages/core/src/lib/schema/typing/result.ts
@@ -4,7 +4,7 @@ type TConfig<T> = {
     [K in keyof T]?: true;
 };
 
-type LPick<T, S extends TConfig<T>> = Pick<T, keyof S & keyof T>;
+type LPickField<T, S extends TConfig<T>> = Pick<T, keyof S & keyof T>;
 
 
 
