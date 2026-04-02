d7925403ff4fdc61ad36db9f7c7427b5e750f9ab
GUMRI <37027572+GUMRI@users.noreply.github.com>
2026-04-02 15:20:06 +0000

    Auto-commit

--- packages/core/src/lib/schema/typing/result.ts
+++ packages/core/src/lib/schema/typing/result.ts
@@ -14,4 +14,4 @@ export interface ShepeOptions<T> {
     omit: TConfig<T>;
 
 }
-export type appShepa<T, TConfig extends ShepeOptions<T>> = PickFiellds<T, TConfig['pick']>
+export type applShepa<T, TConfig extends ShepeOptions<T>> = PickFiellds<T, TConfig['pick']>
