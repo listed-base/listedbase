faa6d00aeaf1ad57e47b6de5ba43e7aa2b0ee08b
GUMRI <37027572+GUMRI@users.noreply.github.com>
2026-04-02 10:17:52 +0000

    Auto-commit

--- packages/core/src/lib/list/listedbase.ts
+++ packages/core/src/lib/list/listedbase.ts
@@ -39,7 +39,7 @@ export function list<S extends TSchemaRef>(
   schema: S,
 ) {
   const store = createReactive<LItem<S>[]>()
-  st
+  store.init([])
   type Item = LItem<S>
   const json = schema.schema.toJSONSchema()
   const props = json.properties as Record<string, any>
