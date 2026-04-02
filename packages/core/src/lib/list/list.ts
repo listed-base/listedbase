a8ca2050056370951f049fc3a958f0f2a3579d44
GUMRI <37027572+GUMRI@users.noreply.github.com>
2026-04-02 15:04:19 +0000

    Auto-commit

--- packages/core/src/lib/list/list.ts
+++ packages/core/src/lib/list/list.ts
@@ -2,7 +2,7 @@ import { z, ZodObject, ZodRawShape } from 'zod/v4';
 import { CreateInput, ListMutationsImpl, UpdateInput } from './mutation';
 import { TReadonlyState } from '@listedbase/uni-reactive';
 import { LFilterInput } from '../filters/types';
-import { TSchemaRefSchemaRef } from '../schema/schema';
+import { TSchemaRe } from '../schema/schema';
 export interface ListRef<TOutput, TInput> {
     items: TReadonlyState<TOutput[]>;
     findUnique: (where: Partial<TOutput>) => TOutput | undefined;
