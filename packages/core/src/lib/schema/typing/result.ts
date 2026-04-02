b341b5df6b79080f49a3bf0aff053b39db53ce16
GUMRI <37027572+GUMRI@users.noreply.github.com>
2026-04-02 15:15:16 +0000

    Auto-commit

--- packages/core/src/lib/schema/typing/result.ts
+++ packages/core/src/lib/schema/typing/result.ts
@@ -10,4 +10,4 @@ type PickFiellds<T, S extends PickConfig<T>> = Pick<T, keyof S & keyof T>;
 export interface FieldsConfig<T> {
     pick: PickConfig<T>;
 }
-export type ItmeShepa<T, TConfig extends FieldsConfig<T>>  = 
+export type ItmeShepa<T, TConfig extends FieldsConfig<T>>  = PickFiellds<T, TConfig['pick']>
