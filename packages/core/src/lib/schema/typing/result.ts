1752c0b744b31520fe714cefcb33e54b8a8876c3
GUMRI <37027572+GUMRI@users.noreply.github.com>
2026-04-02 15:33:03 +0000

    Auto-commit

--- packages/core/src/lib/schema/typing/result.ts
+++ packages/core/src/lib/schema/typing/result.ts
@@ -4,6 +4,10 @@ type TConfig<T> = {
     [K in keyof T]?: true;
 };
 
+export interface ShepeOptions<T> {
+    pick?: TConfig<T>;
+    omit?: TConfig<T>;
+}
 type LPickFields<T, S extends TConfig<T>> = Pick<T, keyof S & keyof T>;
 type LOmitFields<T, S extends TConfig<T>> = Omit<T, keyof S & keyof T>;
 
