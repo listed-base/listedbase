018671f32fc3babb0c3f9663e0824a06dd962329
GUMRI <37027572+GUMRI@users.noreply.github.com>
2026-04-02 10:19:27 +0000

    Auto-commit

--- packages/core/src/lib/list/listedbase.ts
+++ packages/core/src/lib/list/listedbase.ts
@@ -38,7 +38,7 @@ function applySelect<T extends object, S extends SelectConfig<T>>(
 export function list<S extends TSchemaRef>(
   schema: S,
 ) {
-  type item = LItem<S>| A
+  type item = LItem<S>| ApplySelect
   const store = createReactive<LItem<S>[]>()
   store.init([])
   type Item = LItem<S>
