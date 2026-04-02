3853d1b7f68d28ac5592db9d0eb6725300d3a9d1
GUMRI <37027572+GUMRI@users.noreply.github.com>
2026-04-02 10:45:03 +0000

    Auto-commit

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -2,5 +2,5 @@ export * from './lib/list/listedbase';
 // export * from './lib/filters/filter';
 export * from './lib/schema/schema';
 export * from './lib/schema/field';
-export * from './lib/schema/typing/i';
+export * from './lib/schema/typing/itme';
 // export * from './lib/schema/pupolar';
